75bcf0b5c45af12adba6f7d090a743c5977a8e92
Mac Mini 2011 <macmini2011@makkunoMac-mini.local>
2020-07-15 12:02:51 +0900

    remake in English

--- fukubiki.py
+++ fukubiki.py
@@ -15,8 +15,8 @@ logging.basicConfig(level=logging.DEBUG) if DebugMode else logging.basicConfig(l
 Main codes area
 """
 class box():
-    def __init__(self, number_of_ball=100, win=2):
-        self.NoB = number_of_ball
+    def __init__(self, number_of_balls=100, win=2):
+        self.NoB = number_of_balls
         self.win = win
         self.lose = self.NoB - self.win
         self.result=[]
@@ -26,9 +26,9 @@ class box():
         for i in range(self.lose):
             self.balls.append("lose")
         
-    def pick_ball(self,times=1, presult=True, wait=3):
+    def pick_ball(self,times=1, presult=False, wait=3):
         if len(self.balls)<times:
-            if presult: print("玉が足りません...")
+            if presult: print("Balls are not enough...")
             logging.error({"error":"Not enough to take the number of balls you assigned."})
         else:
             for i in range(times):
@@ -38,20 +38,20 @@ class box():
                 r=self.balls.pop(n)
                 if r=="win":
                     logging.info({"info":"Win"})
-                    if presult: print("当たりです！！おめでとう！！")
+                    if presult: print("You win! Congratulations!")
                 else:
                     logging.info({"info":"Lose"})
-                    if presult: print("残念...はずれ...")
+                    if presult: print("You lose...Sorry...")
                 self.result.append(r)
                 if len(self.balls)==0:
-                    logging.warning({"warning":"The balls are gone."})
-                    if presult: print("玉がなくなりました")
+                    logging.warning({"warning":"All balls are gone."})
+                    if presult: print("All balls are gone.")
 
 
 """
 Test codes area
 """
 if __name__=="__main__":
-    box=box(number_of_ball=3, win=1)
-    box.pick_ball(times=3, presult=False, wait=1)
+    box=box(number_of_balls=3, win=1)
+    box.pick_ball(times=3, presult=True, wait=1)
     box.pick_ball()
